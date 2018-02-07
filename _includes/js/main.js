6390326358de7de4062c94a2bc8d0b760b51436e
ZJun <zhangjun310@live.com>
2018-02-07 16:17:01 +0800

    math

--- _includes/js/main.js
+++ _includes/js/main.js
@@ -1,3 +1,4 @@
 jQuery(document).ready(function($){
     // todo: jquery stuff should need to put here.
+    <script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
 });
